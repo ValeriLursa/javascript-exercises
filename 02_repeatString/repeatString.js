0c069b3d861697b20c3167b89d9a64d60c853d61
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:02:15 +0300

    Добавление решения ко второй задаче

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (str == '') return '';
+    if (count == -1) return 'ERROR';
+    if (count == 0) return '';
+    if (count - 1 == 0) return str;
+    if (count > 0) return str + repeatString(str, --count);
 };
 
 // Do not edit below this line
