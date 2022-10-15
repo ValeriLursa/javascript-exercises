5677763b52e0d9bb68c03d060a0f4b003ced8bce
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:03:50 +0300

    Добавление решения к задаче 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+  return Math.abs(first - second);
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length() == 0) return 0;
+  var sum = 0;
+  array.forEach(elem => {
+    sum += elem;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  if (array.length() == 0) return 0;
+  var mul = 0;
+  array.forEach(elem => {
+    mul *= elem;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function (base, degree) {
+  if (degree == 0) return base;
+  if (degree > 0) base * power(base, --degree);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0) return 1;
+  if (number == 1) return number;
+  if (number > 1) number * factorial(--number);
 };
 
 // Do not edit below this line
