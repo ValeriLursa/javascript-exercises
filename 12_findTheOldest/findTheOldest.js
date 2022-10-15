af0672186908fed3d1b810a0648fe84b6765d4b1
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:04:40 +0300

    Добавление решения к задаче 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let arrayDifference = [];
+    const data = new Date();
 
+    people.forEach(person => {
+        if (person.hasOwnProperty('yearOfDeath'))
+            arrayDifference.push(person.yearOfDeath - person.yearOfBirth);
+        else arrayDifference.push(data.getFullYear() - person.yearOfBirth);
+    });
+    console.log(arrayDifference);
+    return people[arrayDifference.indexOf(Math.max.apply(null, arrayDifference))];
 };
 
 // Do not edit below this line
