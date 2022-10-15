006c6795ce4378eb539daeeb6d686d2a3efe20fd
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:03:05 +0300

    Добавление решения к задаче 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function (firstArg, secondArg) {
+    if (firstArg * secondArg < 0) return 'ERROR';
+    if (!Number.isInteger(firstArg) && !Number.isInteger(secondArg)) return 'ERROR';
+    return (firstArg + secondArg) * (Math.abs(secondArg - firstArg) + 1) / 2;
 };
 
 // Do not edit below this line
