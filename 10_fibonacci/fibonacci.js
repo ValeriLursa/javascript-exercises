f6e3c95336c9f3701a6122393ebddaaf780bb0b5
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:04:14 +0300

    Добавление решения к задаче 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    let firstElem = 0;
+    let secondElem = 1;
+    return findNumbersElem(firstElem, secondElem, number);
 };
+
+function findNumbersElem(firstElem, secondElem, number){
+    if (number == 0) return 0;
+    if (number == 1) return secondElem;
+    if (number > 1) return findNumbersElem(secondElem, firstElem + secondElem, --number);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
