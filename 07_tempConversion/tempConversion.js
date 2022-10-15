f6fbf8d784d0e15b575e1640fdef855985586fd6
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:03:32 +0300

    Добавление решения к задаче 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (grad) {
+  let result = (grad - 32) * 5 / 9;
+  if (!Number.isInteger(result)) result.toFixed(1);
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function (grad) {
+  let result = grad * 9 / 5 +32;
+  if (!Number.isInteger(result)) result.toFixed(1);
+  return result;
 };
 
 // Do not edit below this line
