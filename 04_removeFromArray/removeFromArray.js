1598cecb323bbf72e211d20e443f03f174d7542b
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-16 01:02:52 +0300

    Добавление решения к четвертой задаче

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    args.forEach(arg => {
+        array.splice(array.indexOf(arg),1);
+    });
+    return array;
 };
 
 // Do not edit below this line
